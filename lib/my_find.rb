ef9c6d94615439f3223ba64ae3932207b87e735f
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-13 19:44:43 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,9 @@ def my_find(collection)
   i = 0 
   collection = []
   while i < collection.length 
-    if yield(collection[i])
-      return collection[i]
+     return collection[i] if
+    yield(collection[i])
+      i += 1 
     end
-    i += 1 
   end
 end
